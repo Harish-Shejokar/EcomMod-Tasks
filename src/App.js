b21dfb8c5b328293d14e7a94d03218cbff2aabb8
Harish-Shejokar <hshejokar@gmail.com>
2023-04-07 16:27:14 +0530

    fetch by async-await

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fethMoviesHandler = () => {
-    fetch("https://swapi.dev/api/films/").then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        // console.log(data.results)
-        const transformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        setMovies(transformedMovies);
-      })
-      .catch((err) => console.log(err));
+  const fethMoviesHandler = async () => {
+    try {
+      const response = await fetch("https://swapi.dev/api/films/")
+      const data = await response.json();
+      // console.log(data.results)
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    }
+    catch (err) {
+      console.log(err)
+    }
   }
- 
   
 
 
